084c35ffa7a442d72a0a1f6b6b6fae7a113c507c
+ <jkunins95@gmail.com>
2023-02-13 10:01:56 -0500

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var employee = new Object();
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    return Object.assign({}, object, {[key]: value})
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key] = value
+    return object
+}
+
+function deleteFromEmployeeByKey(object, key) {
+    var newEmployee = Object.assign({}, object)
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key) {
+    delete employee[key]
+    return employee
+}
